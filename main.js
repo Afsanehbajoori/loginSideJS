c3a9f98fa125c281d6a9fab242f61c4bd1c50328
Afsaneh Bajoori <afsan.bajoori@hotmail.com>
2021-11-09 14:52:51 +0100

    loocalstorage

--- main.js
+++ main.js
@@ -1,14 +1,18 @@
 var btn = document.getElementById('btn');
 var mainDiv= document.getElementById('mainDiv');
 var txt=document.getElementById('txt');
+localStorage.setItem('usename' , 'user1');
+localStorage.setItem('password' ,'user1');
 
 
 btn.addEventListener('click' , (e) =>{
    e.preventDefault(); 
    var username=mainDiv.username.value;
    var password=mainDiv.password.value;
+   const udata=localStorage.getItem('username');
+   const pdata=localStorage.getItem('password');
 
-   if(username === "user" && password === "hi")
+   if(username === "user1" && password === "user1")
    {
        alert('successful');
    }
