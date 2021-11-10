0ff1e7bba420fd673611e540d2a67619d3c97949
Afsaneh Bajoori <afsan.bajoori@hotmail.com>
2021-11-10 14:05:05 +0100

    install flask

--- serverSide.py
+++ serverSide.py
@@ -8,7 +8,8 @@ def form():
 
 @app.route('/hello', methods=['GET', 'POST'])
 def hello():
-    return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
+    return render_template('greeting.html')
+    # return render_template('greeting.html', say=request.form['say'], to=request.form['to'])
 
 if __name__ == "__main__":
     app.run()
