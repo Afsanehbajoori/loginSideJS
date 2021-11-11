60fee4eee3c617c82222e63cd4e0676a1af6172d
Afsaneh Bajoori <afsan.bajoori@hotmail.com>
2021-11-11 13:35:51 +0100

    express server virker huuuuurrrrraaaaaaaaaaaa

--- app.js
+++ app.js
@@ -2,20 +2,36 @@ const express = require("express")
 const app = express();
 const path=require('path');
 const router = express.Router();
+//var main = require('./main').main;
 
 
 router.get('/index' , function(req,res){
-    res.sendFile(path.join(__dirname+'index.html'));
+    res.sendFile(__dirname+'/index.html');
     // res.send('index.html');
 });
 
-router.get('/next' , function(req,res){
-    res.sendFile(path.join(__dirname+'next.html'));
+router.get('/stylesheet.css' , function(req,res){
+    res.sendFile(__dirname+'/stylesheet.css')
 });
 
-app.use('/' , router);
-app.listen(process.env.port || 1234);
+router.get('/main.js' , function(req,res){
+    res.sendFile(__dirname+'/main.js')
+});
 
+router.get('/next' , function(req,res){
+    res.sendFile(__dirname+'/next.html');
+});
+
+/* app.use('/' , router);
+app.listen(process.env.port || 1234); */
+
+/*  router.get('/', function(req,res){
+    res.redirect(__dirname+'/index.html')
+}); */
+
+app.use('/' , router);
+
+app.listen(1234);
 console.log('running at port 1234');
 /* let port = 12346;
 app.listen(port , function(){
